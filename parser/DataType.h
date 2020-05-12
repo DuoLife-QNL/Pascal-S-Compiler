9a8fbb3bee13154ac6147543a28ec97715ce3b3c
root <root@Ethan-Laptop.localdomain>
2020-05-12 19:08:22 +0800

    更改符号表结构

--- parser/DataType.h
+++ parser/DataType.h
@@ -10,13 +10,19 @@ typedef struct parameter_list{
   std::string name;
   int type;
   bool isVAR;
+  parameter_list * next_element;
 }parameter_list;
+
+typedef struct parameter_list_head{
+  parameter_list * pl;
+  int ret_type;
+}parameter_list_head;
 
 typedef struct {
     std::string name;
     bool isFunction;
     int type;
-    parameter_list *pl;
+    parameter_list_head *plh;
 
     std::string to_string(){
       return "{" + name + "," + std::to_string(type) + "}";
