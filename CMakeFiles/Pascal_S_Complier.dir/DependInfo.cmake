6f5d7c1bf94b26ebb1a222cb227173190b68dd55
CCDV2 <denghaoyuan1100@126.com>
2020-06-29 12:12:12 +0800

    add some target code gen

---
+++ CMakeFiles/Pascal_S_Complier.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/root/Pascal-S-Complier/parser/IdTable.cpp" "/root/Pascal-S-Complier/CMakeFiles/Pascal_S_Complier.dir/parser/IdTable.o"
+  "/root/Pascal-S-Complier/parser/IdType.cpp" "/root/Pascal-S-Complier/CMakeFiles/Pascal_S_Complier.dir/parser/IdType.o"
+  "/root/Pascal-S-Complier/parser/debug.cpp" "/root/Pascal-S-Complier/CMakeFiles/Pascal_S_Complier.dir/parser/debug.o"
+  "/root/Pascal-S-Complier/parser/parser.tab.cpp" "/root/Pascal-S-Complier/CMakeFiles/Pascal_S_Complier.dir/parser/parser.tab.o"
+  "/root/Pascal-S-Complier/scanner/lex.yy.cpp" "/root/Pascal-S-Complier/CMakeFiles/Pascal_S_Complier.dir/scanner/lex.yy.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ CMakeFiles/Pascal_S_Complier.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "parser/parser.tab.cpp"
+  "scanner/lex.yy.cpp"
+  "CMakeFiles/Pascal_S_Complier.dir/parser/parser.tab.o"
+  "CMakeFiles/Pascal_S_Complier.dir/scanner/lex.yy.o"
+  "CMakeFiles/Pascal_S_Complier.dir/parser/IdTable.o"
+  "CMakeFiles/Pascal_S_Complier.dir/parser/IdType.o"
+  "CMakeFiles/Pascal_S_Complier.dir/parser/debug.o"
+  "Pascal_S_Complier.pdb"
+  "Pascal_S_Complier"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Pascal_S_Complier.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
