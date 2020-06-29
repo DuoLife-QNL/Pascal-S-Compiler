895dab3b44e7458b6556de4a6d2652732b167327
Halloweenwx <halloweenwx@163.com>
2020-06-29 21:21:55 +0800

    1) mv member ret_type from Id to Block 2) make is_var_ & is_const_ public (make sure their Derived classes can visit them without public methods) 3) add void type as ret_type for procedure 3) reformat
    Signed-off-by: Halloweenwx <halloweenwx@163.com>

--- parser/IdType.h
+++ parser/IdType.h
@@ -6,14 +6,13 @@
 #include <string>
 #include <vector>
 
-// #define error_report(s) fprintf(stderr, s)
-
 /*
  * We add '_' before each type because they have 
  * been declared as tokens in parser.y
  */
 typedef enum TYPE {
   _DEFAULT,
+  _VOID,
   _INTEGER,
   _REAL,
   _BOOLEAN,
@@ -21,100 +20,93 @@ typedef enum TYPE {
   _ARRAY,
   _PROCEDURE,
   _FUNCTION
-}TYPE;
-
+} TYPE;
 
 class Id {
-  private:
-    TYPE type;
-    std::string name;
-    TYPE ret_type;
+ private:
+  TYPE type;
+  std::string name;
 
-  public:
-    Id(std::string name, TYPE type, TYPE ret_type);
-    std::string get_name();
-    TYPE get_type();
-    TYPE get_ret_type();
+ public:
+  Id(std::string name, TYPE type);
+
+  TYPE get_type();
+  std::string get_name();
 };
 
 /* 
  * according to PPT, basic type include:
  * integer, real, boolean, char
  */
-class BasicTypeId: public Id {
-  private:
-    bool is_const_;
-  public:
-    BasicTypeId(std::string name, TYPE type, bool is_const);
-    bool is_const();
+class BasicTypeId : public Id {
+ public:
+  bool is_const;
+  BasicTypeId(std::string name, TYPE type, bool is_const);
 };
 
 /* struct period stores the start num and end num of a dimension of an array */
-typedef struct period{
+typedef struct period {
   int start;
   int end;
   period *next;
-}period;
+} period;
 /* initiate a new period, and return the pointer */
 period *init_period();
 /* append a new period to the existed period list */
 void append_period(period *target_period, period *new_period);
 
-class ArrayId: public Id {
-  private:
-    /* dimension */
-    int dim;
-    period *prd;
-    TYPE element_type;
-  
-  public:
-    /* Constructor:
-     * @et: type of elements in this array
-     * @prd is an pointer to the array period list 
-     */
-    ArrayId(std::string name, TYPE et, int dim, period *prd);
-    ~ArrayId();
-    int get_dim();
-    period get_period(int dim);
+// multi-dimension array
+class ArrayId : public Id {
+ private:
+  /* dimension */
+  int dim;
+  period *prd;
+  TYPE element_type;
+
+ public:
+  /* Constructor:
+   * @et: type of elements in this array
+   * @prd is an pointer to the array period list
+   */
+  ArrayId(std::string name, TYPE et, int dim, period *prd);
+  ~ArrayId();
+  int get_dim();
+  period get_period(int dim);
 };
 
-
-class Parameter: public BasicTypeId{
-  private:
-    /*
-    * @isVAR: is var_Parameter
-    * false if value_Parameter
-    */
-    bool is_var_;
-  
-  public:
-    Parameter(std::string name, TYPE type, bool is_var);
-    bool is_var();
-
+class Parameter : public BasicTypeId {
+ public:
+  /*
+  * @isVAR: is var_Parameter
+  * false if value_Parameter
+  */
+  bool is_var;
+  Parameter(std::string name, TYPE type, bool is_var);
 };
 
 /* function and procedure are inherited from Block */
-class Block: public Id {
-  private:
-    /* Parameter list */
-    std::vector<Parameter>pl; 
-  public:
-    Block(std::string name, TYPE type, std::vector<Parameter> pl, TYPE ret_type);
-    /* get Parameter list */
-    std::vector<Parameter> get_par_list();
+class Block : public Id {
+ private:
+  TYPE ret_type;
+  /* Parameter list */
+  std::vector<Parameter> pl;
+ public:
+
+  Block(std::string name, TYPE type, std::vector<Parameter> pl, TYPE ret_type);
+  /* get Parameter list */
+  std::vector<Parameter> get_par_list();
+
+  TYPE get_ret_type();
 };
 
-class ProcedureId: public Block {
-  public:
-    ProcedureId(std::string name, std::vector<Parameter> pl);
+class ProcedureId : public Block {
+ public:
+  ProcedureId(std::string name, std::vector<Parameter> pl);
 };
 
-class FunctionId: public Block {
-  private:
-    /* return type */
-    // TYPE ret_type;
-  public:
-    FunctionId(std::string name, std::vector<Parameter> pl, TYPE ret_type);
+class FunctionId : public Block {
+ public:
+  FunctionId(std::string name, std::vector<Parameter> pl, TYPE ret_type);
 };
 
 #endif // STRUCT_DEF_H
