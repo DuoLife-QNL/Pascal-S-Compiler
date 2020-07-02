79caf43eb1bad09b9e72d008b92084cb09a3b113
Hou Weiying <weiying_hou@outlook.com>
2020-07-02 20:45:37 +0800

    1) error recovery for idlist
    Signed-off-by: Hou Weiying <weiying_hou@outlook.com>

---
+++ test/idlist_e.pas
@@ -0,0 +1,13 @@
+program idTest(input, output);
+const
+  name = 'a';
+var
+  x, y;
+procedure out(2a: real; b: boolean; c: char);
+begin
+end;
+
+begin
+  //        read(x, y);
+  //        write(gcd(x, y))
+end.
