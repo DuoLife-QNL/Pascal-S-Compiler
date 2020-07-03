411577d7b5febb5f7f0f3cb327859a4eb9f8b38d
Hongzheng-Li <Ethan.Lee.QNL@gmail.com>
2020-07-03 22:59:32 +0800

    添加快排测试代码

---
+++ test/quick_sort.pas
@@ -0,0 +1,55 @@
+
+Program test(input, output);
+
+Var a: array[1..101] Of integer;
+  k,
+  m,tempOut: integer;
+Function partition(low,high: integer): integer;
+
+Var i, j, temp: integer;
+Begin
+  i := low - 1;
+  For j:=low To high Do
+    Begin
+      If a[j] <= a[high] Then
+        Begin
+          i := i + 1;
+          temp := a[i];
+          a[i] := a[j];
+          a[j] := temp
+        End;
+      j := j + 1
+    End ;
+  i := i + 1;
+  temp := a[i];
+  a[i] := a[high];
+  a[high] := temp;
+  partition := i
+End;
+Procedure qs(low,high: integer);
+
+Var pivot: integer;
+Begin
+  pivot := 0;
+  If low <= high Then
+    Begin
+      pivot := partition(low, high);
+      qs(low, pivot - 1);
+      qs(pivot + 1, high)
+    End
+End;
+Begin
+  read(m);
+  For k:=1 To (m + 1) Do
+    Begin
+      read(tempOut);
+      a[k] := tempOut;
+      k := k+1
+    End;
+  qs(1, m);
+  For k:=1 To (m+1) Do
+    Begin
+      write(a[k]);
+      k := k + 1
+    End
+End.
