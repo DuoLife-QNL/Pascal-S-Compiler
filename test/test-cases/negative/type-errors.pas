3718e281912b65b275b9afc9b2bfd7e97b5befaf
Hongzheng-Li <Ethan.Lee.QNL@gmail.com>
2020-07-04 20:16:38 +0800

    update test cases

---
+++ test/test-cases/negative/type-errors.pas
@@ -0,0 +1,15 @@
+
+Program test(input, output);
+
+Var int: integer;
+  bool : boolean;
+Begin
+  If bool = int Then
+    int := 1
+  Else
+    Begin
+      int := int + bool;
+      int := bool;
+      int := 2
+    End
+End.
