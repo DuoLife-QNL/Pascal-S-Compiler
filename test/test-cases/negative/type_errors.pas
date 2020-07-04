2cc9f5d8f472c6071063e0d54ed662e894c9816b
Hongzheng-Li <Ethan.Lee.QNL@gmail.com>
2020-07-05 00:16:39 +0800

    update type error

--- test/test-cases/negative/type_errors.pas
+++ test/test-cases/negative/type_errors.pas
@@ -12,11 +12,13 @@ Begin
       int := bool;
       int := 2
     End;
-  For bool := 1 To 2.3 Do Begin
-    if 1 + 2 Then
-      int := 3
-  End
-  For int := 1.1 To 2.3 Do Begin
-    int := 2
-  End
+  For bool := 1 To 2.3 Do
+    Begin
+      If 1 + 2 Then
+        int := 3
+    End;
+  For int := 1.1 To 2.3 Do
+    Begin
+      int := 2
+    End
 End.
