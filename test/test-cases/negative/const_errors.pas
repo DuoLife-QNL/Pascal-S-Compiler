f927b771c31a70a6ff7b2c6adebf04b93be13ac2
Hongzheng-Li <Ethan.Lee.QNL@gmail.com>
2020-07-05 00:05:46 +0800

    update const errors

--- test/test-cases/negative/const_errors.pas
+++ test/test-cases/negative/const_errors.pas
@@ -1,9 +1,14 @@
+
 Program const_errors(input, output);
-Const
+
+Const 
   b = 1;
   a = 3;
 Begin
   read(b);
-  for b := 1 to 3 do
-    a := 4
+  For b := 1 To 3 Do
+    Begin
+      a := 4;
+      b := 3;
+    End;
 End.
