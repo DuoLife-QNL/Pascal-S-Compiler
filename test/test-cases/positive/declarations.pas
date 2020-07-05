46f5ad499d005de2e5da80e8cd5351a82ac87754
Hongzheng-Li <Ethan.Lee.QNL@gmail.com>
2020-07-05 16:25:30 +0800

    update declaration test case

--- test/test-cases/positive/declarations.pas
+++ test/test-cases/positive/declarations.pas
@@ -2,6 +2,7 @@
 Program declarations(input, output);
 
 Const c = 1;
+      ch = '#';
 
 Var x,y,sum1, sum2, sum3: integer;
   z: array[1..2] Of integer;
@@ -12,9 +13,10 @@ Begin
 End;
 Begin
   read(x, y);
-  z[1] := 1;
-  z[2] := 2;
+  z[1] := 3;
+  z[2] := 4;
   sum1 := sum(x, y);
   sum2 := sum(z[1], z[2]);
+  write(ch);
   write(sum(sum1, sum2));
 End.
