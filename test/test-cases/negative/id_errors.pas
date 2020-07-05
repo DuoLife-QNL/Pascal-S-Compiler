f6d69c8d0b16c894ef2fd678595a40c8c2a62c89
Hongzheng-Li <Ethan.Lee.QNL@gmail.com>
2020-07-05 11:24:59 +0800

    update test cases

--- test/test-cases/negative/id_errors.pas
+++ test/test-cases/negative/id_errors.pas
@@ -3,9 +3,13 @@ Const
   b = 1;
 
 Var
+  // 重复定义
   a, x, x, y: integer;
+  // 重复定义
   a: integer;
 Begin
+  // 运算对象缺失
   x := 3 +;
+  // 未定义标识符
   read(r, y)
 End.
--- test/test-cases/negative/operation_errors.pas
+++ test/test-cases/negative/operation_errors.pas
@@ -3,11 +3,13 @@ Program operation_errors(input, output);
 
 Var x,y,z: integer;
 Begin
-x:= 0;
-y:= 1;
-  //缺少运算对象
+  x := 0;
+  y := 1;
+  // 缺少运算对象
   z := x + ;
-  //运算符缺失
+  // 运算符缺失
   z := x y;
+  // 括号不匹配
+  z := (x+y;
   read(x, y)
 End.
