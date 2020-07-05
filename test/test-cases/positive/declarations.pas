dc233302f586bff1e9f5742e638261352304a439
Hongzheng-Li <Ethan.Lee.QNL@gmail.com>
2020-07-05 16:31:15 +0800

    更新测试用例

--- test/test-cases/positive/declarations.pas
+++ test/test-cases/positive/declarations.pas
@@ -1,7 +1,7 @@
 
 Program declarations(input, output);
 
-Const c = 1;
+Const c = +10;
       ch = '#';
 
 Var x,y,sum1, sum2, sum3: integer;
@@ -17,6 +17,8 @@ Begin
   z[2] := 4;
   sum1 := sum(x, y);
   sum2 := sum(z[1], z[2]);
+  write(ch);
+  write(c);
   write(ch);
   write(sum(sum1, sum2));
 End.
